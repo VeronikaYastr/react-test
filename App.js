a047d9af5d6f6718133ec3e059fe3ac43baa1f06
vyastrebova <veronika.yastrebova@pstlabs.by>
2020-05-19 18:07:31 +0300

    'first'

--- App.js
+++ App.js
@@ -1,36 +1,169 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import * as React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {FlatList, ImageBackground, Text, TouchableOpacity, View, Image} from 'react-native';
+import {Camera} from 'expo-camera';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-import useCachedResources from './hooks/useCachedResources';
-import BottomTabNavigator from './navigation/BottomTabNavigator';
-import LinkingConfiguration from './navigation/LinkingConfiguration';
 
-const Stack = createStackNavigator();
+export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [camera, setCamera] = useState(null);
+  const [pictureUri, setPictureUri] = useState(null);
 
-export default function App(props) {
-  const isLoadingComplete = useCachedResources();
+  useEffect(() => {
+    (async () => {
+      const {status} = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
 
-  if (!isLoadingComplete) {
-    return null;
-  } else {
+  const snapPhoto = async () => {
+    if (camera) {
+      let photo = await camera.takePictureAsync();
+      setPictureUri(photo.uri);
+    }
+  };
+
+  const backToCamera = () => {
+    setPictureUri(null);
+  };
+
+  if (pictureUri) {
     return (
-      <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="dark-content" />}
-        <NavigationContainer linking={LinkingConfiguration}>
-          <Stack.Navigator>
-            <Stack.Screen name="Root" component={BottomTabNavigator} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </View>
+      <ImageBackground
+        style={{
+          flex: 1,
+          flexDirection: 'column',
+          paddingHorizontal: 10,
+          paddingVertical: 30
+        }}
+        source={{uri: pictureUri}}>
+        <TouchableOpacity
+          style={{
+            flex: 15,
+            alignSelf: 'flex-start',
+          }}
+          onPress={backToCamera}>
+          <Icon name="times-circle" size={70} color="white"/>
+        </TouchableOpacity>
+        <Models
+          style={{
+            flex: 1,
+            alignSelf: 'flex-end',
+          }}
+        />
+      </ImageBackground>
     );
   }
+
+  if (hasPermission === null) {
+    return <View/>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+  return (
+    <View style={{flex: 1}}>
+      <Camera
+        style={{flex: 1}}
+        type={type}
+        ref={ref => setCamera(ref)}>
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: 'transparent',
+            flexDirection: 'row',
+          }}>
+          <TouchableOpacity
+            style={{
+              flex: 1,
+              alignSelf: 'flex-end',
+              marginBottom: 35,
+              alignItems: 'center',
+            }}
+            onPress={snapPhoto}>
+            <Icon name="circle" size={70} color="white"/>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+function Item({id, title, selected, onSelect}) {
+  return (
+    <TouchableOpacity
+      onPress={() => onSelect(id)}
+      style={{
+        flex: 1,
+        flexDirection: 'column',
+        height: 140,
+        width: 110,
+        backgroundColor: 'white',
+        borderRadius: 7,
+        marginHorizontal: 5,
+      }}
+    >
+      <Image
+        style={{
+          height: 110,
+          borderTopLeftRadius: 7,
+          borderTopRightRadius: 7,
+          width: 110
+        }}
+        source={ require("./assets/images/s1200.jpg") }
+      />
+      <Text
+        style={{
+          height: 30,
+          textAlign: 'center',
+          width: 110
+        }}
+      >{title}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
   },
-});
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+  {
+    id: 'dscxzcz23-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'Forth Item',
+  },
+  {
+    id: '3acdsafc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '586ewa0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+];
+
+function Models() {
+  return (
+    <FlatList
+      data={DATA}
+
+      horizontal={true}
+      renderItem={({item}) => (
+        <Item
+          id={item.id}
+          title={item.title}
+        />
+      )}
+      keyExtractor={item => item.id}
+    />
+  )
+}
